844740071f975d3a8e55b687a7983165e4114a88
miracuthbert <miracuthbert@gmail.com>
2018-11-28 00:24:58 +0300

    Update Property Factory
    Fix attribute values

--- database/factories/PropertyFactory.php
+++ database/factories/PropertyFactory.php
@@ -4,12 +4,13 @@ use Faker\Generator as Faker;
 
 $factory->define(Smartville\Domain\Properties\Models\Property::class, function (Faker $faker) {
     return [
-        'name' => uniqid(request()->tenant()->short_name),
+        'name' => uniqid(true),
+        'slug' => uniqid(true),
         'address' => $faker->unique()->address,
         'image' => null,
         'overview_short' => $faker->text(160),
         'overview' => $faker->paragraphs(4, true),
-        'currency' => \request()->tenant()->currency,
+        'currency' => 'USD',
         'size' => $faker->randomNumber(4),
         'price' => $faker->numberBetween(1000000, 1500000),
         'live' => true,
