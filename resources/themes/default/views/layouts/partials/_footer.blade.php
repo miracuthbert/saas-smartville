58513b40e1e07aea14cb0df5b148d263f6214163
miracuthbert <miracuthbert@gmail.com>
2018-11-28 01:54:59 +0300

    Update default theme footer partial view
    Remove the bottom margin that is added by jumbotron class

--- resources/themes/default/views/layouts/partials/_footer.blade.php
+++ resources/themes/default/views/layouts/partials/_footer.blade.php
@@ -1,5 +1,5 @@
 <!-- Footer -->
-<div class="jumbotron jumbotron-fluid bg-dark text-white">
+<div class="jumbotron jumbotron-fluid mb-0 bg-dark text-white">
     <div class="container">
         <div class="row justify-content-center">
             &copy; {{ date('Y') }} {{ config('app.name') }}. All rights reserved.
