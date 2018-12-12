fa2013fe89e98934b77f4715b65a64e12ce4977c
miracuthbert <miracuthbert@gmail.com>
2018-12-12 18:20:22 +0300

    Update User resource
    Changed User model json resource response attributes

--- app/Domain/Users/Resources/UserResource.php
+++ app/Domain/Users/Resources/UserResource.php
@@ -14,6 +14,13 @@ class UserResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'timezone' => $this->timezone,
+            'last_login_at' => $this->local_last_login_at,
+            'created_at' => $this->local_created_at,
+        ];
     }
 }
