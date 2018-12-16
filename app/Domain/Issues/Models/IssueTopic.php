542c965b598da7df7d52140e071bb0e424549084
miracuthbert <miracuthbert@gmail.com>
2018-12-16 17:42:46 +0300

    Add issue relationship to IssueTopic model
    Updated IssueTopic model to include an issue relationship

--- app/Domain/Issues/Models/IssueTopic.php
+++ app/Domain/Issues/Models/IssueTopic.php
@@ -7,6 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class IssueTopic extends Model
 {
     /**
+     * Get the issue that owns the topic.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function issue()
+    {
+        return $this->belongsTo(Issue::class);
+    }
+
+    /**
      * Get all of the owning issueable models.
      *
      * @return \Illuminate\Database\Eloquent\Relations\MorphTo
